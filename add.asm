3ed85d2bd86694202c552789865a1419bd83525c
Artem Inkin <arteminkin@yandex.ru>
2025-03-06 20:47:22 +0400

    Update add.asm

--- add.asm
+++ add.asm
@@ -1,1 +1,15 @@
-touch my_assembly_code.asm
+section .data
+msg db 'Hello, World!', 0
+
+section .text
+global _start
+
+_start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, 13
+    int 0x80
+
+    mov eax, 1
+    int 0x80
